71fee2e84e4bc69aeb83c959aede810575babbe3
brady-etzkorn <bre30104@gmail.com>
2026-09-23 11:41:56 -0400

    Update simple_sorts.py

--- simple_sorts.py
+++ simple_sorts.py
@@ -7,13 +7,29 @@
 # Modified by: 
 
 def bubble_sort(lst):
-    # YOUR CODE HERE
-    pass
+    n = len(lst)
+    for i in range(n):
+        for j in range(n - i - 1):
+            if lst[j] > lst[j + 1]:
+                lst[j], lst[j + 1 = lst[j + 1], lst[j]
 
 def selection_sort(lst):
-    # YOUR CODE HERE
-    pass
+    n = len(lst)
+    for i in range(n):
+        mid_index = i
+        for j in range(i + 1, n):
+            if lst[j] < lst[mid_index]:
+                mid_index = j
+        lst[i], lst[mid_index] = lst[mid_index], lst[i]
+            
 
 def insertion_sort(lst):
-    # YOUR CODE HERE
-    pass
+    n = len(lst)
+    for i in range(1, n):
+        key = lst[i]
+        j = i - 1
+        while j >= 0 and lst[j] > key:
+            lst[j + 1] = lst[j]
+            j -= 1
+        lst[j + 1] = key
+    
