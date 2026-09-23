1d0db13c7add7948ba49ac6e1b75a2b156c52859
brady-etzkorn <bre30104@gmail.com>
2026-09-23 11:49:47 -0400

    Update simple_sorts.py

--- simple_sorts.py
+++ simple_sorts.py
@@ -11,7 +11,7 @@ def bubble_sort(lst):
     for i in range(n):
         for j in range(n - i - 1):
             if lst[j] > lst[j + 1]:
-                lst[j], lst[j + 1 == lst[j + 1], lst[j]
+                lst[j], lst[j + 1] == lst[j + 1], lst[j]
 
 def selection_sort(lst):
     n = len(lst)
